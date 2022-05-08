049efd319cbe66741087c0589c6bfd2afc980c17
DocWho22 <68487755+DocWho22@users.noreply.github.com>
2022-05-08 17:17:15 +0800

    更改生理期周期

--- HXbirthday.js
+++ HXbirthday.js
@@ -20,7 +20,7 @@ $.config = {
   username: "胡湘", // 姓名
   birthday: "2000-5-25", // 生日日期
   physiologicalDefault: "2022-4-6", // 最近一次来周期时间
-  physiologicalCycle: "2022-5-3", // 下一次周期
+  physiologicalCycle: "27", // 下一次周期
   nongli: true, // 农历生日
   eday: "2022-2-7",
   loveWords: true,
